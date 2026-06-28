eee0e86c017313304356dff27ea234fe3f3015b6
Mohamed-Marzaban <mohamedshady2003@gmail.com>
2026-06-28 19:11:36 +0300

    feat: add warehouses module with create and list endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { WarehousesModule } from './warehouses/warehouses.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ProductsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ProductsModule, WarehousesModule],
 })
 export class AppModule {}
